3f3c11b80692b89c444aa989dac143fb4fec6f60
kalyanibajirao <kalyanibajirao1999@gmail.com>
2024-09-03 21:48:00 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,13 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import "../styles/App.css";
+import Menu from "./Menu";
 
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
